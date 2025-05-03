b588f6c5ac7abc2ec449988f70b6d982368895b2
MurrayL <15075819+mlohoar@users.noreply.github.com>
2025-05-03 14:38:32 +0100

    version update 2025050301

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin             = new stdClass();
 
-$plugin->version    = 2023041601;
+$plugin->version    = 2025050301;
 $plugin->requires   = 2018120300;
 $plugin->release    = "0.3";
 $plugin->component  = 'local_pluginsbackup';
